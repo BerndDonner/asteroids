b4806937cec95f78d1bdee6f97e539ce9fd83a03
Bernd Donner <bernd.donner@sabel.com>
2024-11-29 20:02:43 +0100

    The player is drawn and can move

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
 # show nixos version:      nixos-version
 # 
 {
-  description = "Boot.dev Environment for Nix 24.05";
+  description = "Boot.dev Environment for Nix 24.11";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     print("Starting asteroids!")
@@ -16,7 +21,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for i in updatable:
+            i.update(dt)
+            
         screen.fill(pygame.Color("black"))
+        
+        for j in drawable:
+            j.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
