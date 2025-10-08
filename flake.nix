1ed474c91393d804ebc1d1e43182875bda0d2d99
Bernd Donner <bernd.donner@sabel.com>
2025-10-08 10:07:11 +0200

    bootdev cammand works again

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 # show nixos version:      nixos-version
 # 
 {
-  description = "Clean Python + Pygame development environment (pure Nix)";
+  description = "Python + Pygame + Boot.dev dev environment (pure Nix)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
@@ -18,25 +18,26 @@
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default = let
-      # Import nixpkgs for this system
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
       pkgs.mkShell {
-        name = "python-pygame-env";
+        name = "python-pygame-bootdev-devshell";
 
-        # All tools and libraries available in the shell
+        # All development tools go here
         packages = with pkgs; [
           python311
           python311Packages.pygame
-          go
+          (pkgs.callPackage ./bootdev.nix {})
         ];
 
         shellHook = ''
           echo
-          echo "🐍 Python + 🎮 Pygame environment ready"
+          echo "🐍 Python + 🎮 Pygame + 🧰 Boot.dev dev environment ready"
           echo "Try: python3 -m pygame.examples.aliens"
+          echo "Try: bootdev --help"
           echo
         '';
       };
   };
 }
+
